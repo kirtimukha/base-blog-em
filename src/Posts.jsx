546b28c47da42c80b9dfb27d4cd2a798048cdde8
stellamax <kirtimukha@nate.com>
2023-05-04 23:04:18 +0900

    [U] staleTime: useQuery 3번째 아규먼트

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -17,9 +17,10 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  //const data = []; //옵션 쿼리키, 커리에 데이터를 가져올 비동기(async) 함수,
+  //const data = [];
+  // useQuery 아규먼트: 쿼리키, 커리에 데이터를 가져올 비동기(async) 함수, staleTime
   // 페치포스트로부터 반환된 데이터가 매핑된다.
-  const {data, isError, error, isLoading} = useQuery("posts", fetchPosts) ;
+  const {data, isError, error, isLoading} = useQuery("posts", fetchPosts, {staleTime: 2000}) ;
   if(isLoading) return <h3>Loading</h3>;
   if(isError) return <h3>oop, Something went wrong!
     <p>{error.toString()}</p>
